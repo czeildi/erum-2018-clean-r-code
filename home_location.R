ac3fa473e914971648e32404dd93d3030d700814
Ildiko Czeller <czeildi@gmail.com>
2018-04-11 20:32:07 +0200

    handle namibia as valid

--- home_location.R
+++ home_location.R
@@ -4,7 +4,7 @@ library("tidyverse")
 # load data ---------------------------------------------------------------
 
 client_meta_data <- read_csv("data/client_meta_data.csv")
-country_meta_data <- read_csv("data/country_meta_data.csv")
+country_meta_data <- read_csv("data/country_meta_data.csv", na = "")
 home_cities <- data.table::fread("gunzip -c data/home_cities_frequent.csv.gz") %>%
     as.tibble()
 
