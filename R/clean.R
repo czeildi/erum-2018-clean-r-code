18c4d50f90a1b2a2a81d065af4e361378b58ccdb
Ildiko Czeller <czeildi@gmail.com>
2018-04-11 21:44:37 +0200

    share of strongest city and num cities for coverage

--- R/clean.R
+++ R/clean.R
@@ -3,9 +3,9 @@ filter_regionless_countries <- function(df) {
 }
 
 filter_missing_city <- function(df) {
-    filter(df, city == "")
+    filter(df, city != "")
 }
 
 filter_small_countries <- function(df, limit = 1000) {
-    filter(df, num_contact >= limit)
+    filter(df, country_population >= limit)
 }
