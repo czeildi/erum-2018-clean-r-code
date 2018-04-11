1b5ef80282ca6954ca3ea9abf0c6c3c48001ff03
Ildiko Czeller <czeildi@gmail.com>
2018-04-11 20:31:46 +0200

    load country meta data
    NA is Namibia, should not be NA

---
+++ home_location.R
@@ -0,0 +1,15 @@
+library("tidyverse")
+
+
+# load data ---------------------------------------------------------------
+
+client_meta_data <- read_csv("data/client_meta_data.csv")
+country_meta_data <- read_csv("data/country_meta_data.csv")
+home_cities <- data.table::fread("gunzip -c data/home_cities_frequent.csv.gz") %>%
+    as.tibble()
+
+# initial exploration -----------------------------------------------------
+
+# possibly use skimr, visdat
+
+country_meta_data[!complete.cases(country_meta_data), ] %>% View
