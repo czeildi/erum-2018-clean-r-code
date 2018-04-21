61cfbac980271ae1b21705aaf0928c75aa1148b5
Ildiko Czeller <czeildi@gmail.com>
2018-04-21 08:34:12 +0200

    delete infra files from cleaned workshop materials

--- infra/vm_setup.sh
+++
@@ -1,7 +0,0 @@
-apt-get install update
-apt install r-base-core
-git clone https://github.com/czeildi/erum-2018-clean-r-code.git
-# or git pull if change since pull
-
-# docker pull czeildi/erum-clean-r-code
-R -f erum-2018-clean-r-code/infra/start_multiple_rstudio.R
