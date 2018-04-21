ca456c9356a973f926ed59f71f6760c3bdd0acab
Ildiko Czeller <czeildi@gmail.com>
2018-04-21 07:27:37 +0200

    bind data folder as readonly to every container

--- infra/start_multiple_rstudio.R
+++ infra/start_multiple_rstudio.R
@@ -2,7 +2,12 @@ container_num <- 5
 port_start <- 20000
 
 for (user_index in 1:container_num) {
-  cmd <- paste0("docker run -d -p ", port_start + user_index, ":8787", "--memory 1g --cpu-shares 5", " czeildi/erum-clean-r-code")
+  cmd <- paste0(
+      "docker run -d -p ", port_start + user_index, ":8787",
+      " --memory 1g --cpu-shares 5",
+      " -v /home/rstudio/data:/home/rstudio/erum-project/data:ro",
+      " czeildi/erum-clean-r-code"
+  )
   print(cmd)
   system(cmd)
 }
