042f19c3df9240cd8c167fa028ea01a7696b012a
Ildiko Czeller <czeildi@gmail.com>
2018-04-22 11:12:03 +0200

    add exploratory plots

--- home_location.R
+++ home_location.R
@@ -1,4 +1,5 @@
 library("tidyverse")
+library("plotly")
 
 source("R/clean.R")
 
@@ -19,3 +20,24 @@ get_cities_with_multiple_coords(home_cities)
 
 glimpse_extreme_regions(home_cities, countries, country_code, city)
 glimpse_extreme_regions(home_cities, countries, country_code)
+
+home_cities %>% 
+  summarize_population(country_code, city, long, lat) %>%
+  filter(num_contact >= 1000) %>%
+  plot_geo(lat = ~lat, lon = ~long) %>%
+  add_markers(
+    size = ~log10(num_contact),
+    color = ~log10(num_contact),
+    text = ~str_c(country_code, city, num_contact, sep = "<br />")
+  ) %>% 
+  colorbar(title = "Population", tickprefix = "10^")
+
+home_cities %>%
+  summarize_population(country_code) %>%
+  attach_country_metadata(countries) %>%
+  plot_geo(locations = ~iso3c) %>% 
+  add_trace(
+    z = ~log10(num_contact), 
+    text = ~paste(country, prettyNum(num_contact, big.mark = " "), sep = "<br />")
+  ) %>%
+  colorbar(title = "Population", tickprefix = "10^")
