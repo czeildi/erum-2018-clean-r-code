895fe2cdebb946f34344ff4fadb8473e6ebf4547
Ildiko Czeller <czeildi@gmail.com>
2018-04-22 11:13:34 +0200

    use 2 spaces for default

--- home_location.R
+++ home_location.R
@@ -12,28 +12,28 @@ countries[!complete.cases(countries), ]
 home_cities[!complete.cases(home_cities), ]
 
 home_cities %>% 
-    group_by(country_code, city) %>% 
-    summarize(num_coord_per_city = n_distinct(long, lat)) %>% 
-    filter(num_coord_per_city > 1)
+  group_by(country_code, city) %>% 
+  summarize(num_coord_per_city = n_distinct(long, lat)) %>% 
+  filter(num_coord_per_city > 1)
 
 # data exploration --------------------------------------------------------
 
 ordered_city_populations <- home_cities %>% 
-    group_by(country_code, city) %>% 
-    summarize(num_contact = sum(num_contact)) %>% 
-    arrange(desc(num_contact))
+  group_by(country_code, city) %>% 
+  summarize(num_contact = sum(num_contact)) %>% 
+  arrange(desc(num_contact))
 
 head(ordered_city_populations) %>% 
-    inner_join(countries, by = "country_code")
+  inner_join(countries, by = "country_code")
 tail(ordered_city_populations) %>% 
-    inner_join(countries, by = "country_code")
+  inner_join(countries, by = "country_code")
 
 ordered_country_populations <- home_cities %>% 
-    group_by(country_code) %>% 
-    summarize(num_contact = sum(num_contact)) %>% 
-    arrange(desc(num_contact))
+  group_by(country_code) %>% 
+  summarize(num_contact = sum(num_contact)) %>% 
+  arrange(desc(num_contact))
 
 head(ordered_country_populations) %>% 
-    inner_join(countries, by = "country_code")
+  inner_join(countries, by = "country_code")
 tail(ordered_country_populations) %>% 
-    inner_join(countries, by = "country_code")
+  inner_join(countries, by = "country_code")
