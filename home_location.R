e3609efe5ca5019860cda280eb3864e586c9ab3f
Ildiko Czeller <czeildi@gmail.com>
2018-04-22 11:15:46 +0200

    use 2 spaces as default

--- home_location.R
+++ home_location.R
@@ -1,41 +1,39 @@
 library("tidyverse")
-library("plotly")
-
-source("R/clean.R")
 
 # load data ---------------------------------------------------------------
 
+# NA is valid country code, stands for Namibia, so should not be read as NA
 countries <- read_csv("data/countries.csv", na = "")
 home_cities <- read_csv("data/home_cities_frequent.csv.gz", na = "")
 
-# data preparation -----------------------------------------------------
+# data preparation -------------------------------------------------------
 
 countries[!complete.cases(countries), ]
 home_cities[!complete.cases(home_cities), ]
 
 home_cities %>% 
-    group_by(country_code, city) %>% 
-    summarize(num_coord_per_city = n_distinct(long, lat)) %>% 
-    filter(num_coord_per_city > 1)
+  group_by(country_code, city) %>% 
+  summarize(num_coord_per_city = n_distinct(long, lat)) %>% 
+  filter(num_coord_per_city > 1)
 
 # data exploration --------------------------------------------------------
 
 ordered_city_populations <- home_cities %>% 
-    group_by(country_code, city) %>% 
-    summarize(num_contact = sum(num_contact)) %>% 
-    arrange(desc(num_contact))
+  group_by(country_code, city) %>% 
+  summarize(num_contact = sum(num_contact)) %>% 
+  arrange(desc(num_contact))
 
 head(ordered_city_populations) %>% 
-    inner_join(countries, by = "country_code")
+  inner_join(countries, by = "country_code")
 tail(ordered_city_populations) %>% 
-    inner_join(countries, by = "country_code")
+  inner_join(countries, by = "country_code")
 
 ordered_country_populations <- home_cities %>% 
-    group_by(country_code) %>% 
-    summarize(num_contact = sum(num_contact)) %>% 
-    arrange(desc(num_contact))
+  group_by(country_code) %>% 
+  summarize(num_contact = sum(num_contact)) %>% 
+  arrange(desc(num_contact))
 
 head(ordered_country_populations) %>% 
-    inner_join(countries, by = "country_code")
+  inner_join(countries, by = "country_code")
 tail(ordered_country_populations) %>% 
-    inner_join(countries, by = "country_code")
+  inner_join(countries, by = "country_code")
--- infra/start_multiple_rstudio.R
+++ infra/start_multiple_rstudio.R
@@ -3,10 +3,10 @@ port_start <- 20000
 
 for (user_index in 1:container_num) {
   cmd <- paste0(
-      "docker run -d -p ", port_start + user_index, ":8787",
-      " --memory 1g --cpu-shares 5",
-      " -v /home/rstudio/data:/home/rstudio/erum-2018-clean-r-code/data:ro",
-      " czeildi/erum-clean-r-code"
+    "docker run -d -p ", port_start + user_index, ":8787",
+    " --memory 1g --cpu-shares 5",
+    " -v /home/rstudio/data:/home/rstudio/erum-2018-clean-r-code/data:ro",
+    " czeildi/erum-clean-r-code"
   )
   print(cmd)
   system(cmd)
