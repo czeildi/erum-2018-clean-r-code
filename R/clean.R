baca69de0e1ab7ab0f549252b88763a3a72191cb
Ildiko Czeller <czeildi@gmail.com>
2018-04-22 10:55:42 +0200

    refactor first part of data exploration

--- R/clean.R
+++ R/clean.R
@@ -1,10 +1,41 @@
-show_rows_with_missing_value <- function(df) {
-    df[!complete.cases(df), ]
+get_rows_with_missing_value <- function(df) {
+  df[!complete.cases(df), ]
 }
 
-show_cities_with_multiple_coords <- function(home_cities) {
-    home_cities %>% 
-        group_by(country_code, city) %>% 
-        summarize(num_coord_per_city = n_distinct(long, lat)) %>% 
-        filter(num_coord_per_city > 1)
+get_cities_with_multiple_coords <- function(home_cities) {
+  home_cities %>% 
+    group_by(country_code, city) %>% 
+    summarize(num_coord_per_city = n_distinct(long, lat)) %>% 
+    ungroup() %>% 
+    filter(num_coord_per_city > 1)
+}
+
+glimpse_extreme_regions <- function(home_cities, countries, ...) {
+  home_cities %>% 
+    summarize_population(...) %>% 
+    filter_extreme_regions_by_population() %>%
+    attach_human_readable_country_metadata(countries)
+}
+
+summarize_population <- function(home_cities, ...) {
+  home_cities %>% 
+    group_by(...) %>% 
+    summarize(num_contact = sum(num_contact)) %>%
+    ungroup()
+}
+
+filter_extreme_regions_by_population <- function(region_populations) {
+  region_populations %>%
+    arrange(desc(num_contact)) %>%
+    {rbind(head(.), tail(.))}
+}
+
+attach_human_readable_country_metadata <- function(df, countries) {
+  df %>%
+    attach_country_metadata(countries) %>%
+    select(country, everything(), -iso3c, -country_code)
+}
+
+attach_country_metadata <- function(df, countries) {
+  inner_join(df, countries, by = "country_code")
 }
--- home_location.R
+++ home_location.R
@@ -1,38 +1,21 @@
 library("tidyverse")
-library("plotly")
 
 source("R/clean.R")
 
 # load data ---------------------------------------------------------------
 
+# NA is valid country code, stands for Namibia, so should not be read as NA
 countries <- read_csv("data/countries.csv", na = "")
 home_cities <- read_csv("data/home_cities_frequent.csv.gz", na = "")
 
 # data preparation -----------------------------------------------------
 
-show_rows_with_missing_value(countries)
-show_rows_with_missing_value(home_cities)
+get_rows_with_missing_value(countries)
+get_rows_with_missing_value(home_cities)
 
-show_cities_with_multiple_coords(home_cities)
+get_cities_with_multiple_coords(home_cities)
 
 # data exploration --------------------------------------------------------
 
-ordered_city_populations <- home_cities %>% 
-    group_by(country_code, city) %>% 
-    summarize(num_contact = sum(num_contact)) %>% 
-    arrange(desc(num_contact))
-
-head(ordered_city_populations) %>% 
-    inner_join(countries, by = "country_code")
-tail(ordered_city_populations) %>% 
-    inner_join(countries, by = "country_code")
-
-ordered_country_populations <- home_cities %>% 
-    group_by(country_code) %>% 
-    summarize(num_contact = sum(num_contact)) %>% 
-    arrange(desc(num_contact))
-
-head(ordered_country_populations) %>% 
-    inner_join(countries, by = "country_code")
-tail(ordered_country_populations) %>% 
-    inner_join(countries, by = "country_code")
+glimpse_extreme_regions(home_cities, countries, country_code, city)
+glimpse_extreme_regions(home_cities, countries, country_code)
