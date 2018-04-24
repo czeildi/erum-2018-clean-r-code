2089f6eff2f31d9a9106b0faa3e9cdaf644b74ac
Ildiko Czeller <czeildi@gmail.com>
2018-04-24 21:53:00 +0200

    add git config for stashing

--- workshop_tools.R
+++ workshop_tools.R
@@ -1,6 +1,7 @@
 proceed_to <- function(next_step_name) {
   repo <- git2r::repository(rprojroot::find_root('erum-2018-clean-r-code.Rproj'))
   git2r::add(repo, ".")
+  git2r::config(user.name = "erum", user.email = "erum@cleancode.com")
   git2r::stash(repo)
   git2r::checkout(repo, next_step_name)
 }
