22ced70c55977c4061c191053571c2d5d5013be3
erum <erum@cleancode.com>
2018-04-29 21:40:39 +0200

    update vm setup commands

--- infra/vm_setup.sh
+++ infra/vm_setup.sh
@@ -1,8 +1,14 @@
-apt-get install update
+ssh root@<ip>
+apt-get update
 apt install r-base-core
 git clone https://github.com/czeildi/erum-2018-clean-r-code.git
+cd erum-2018-clean-r-code
 git checkout infra
 # or git pull if change since pull
+# edit container num in infra/start_multiple_rstudio.R
+R -f ./infra/start_multiple_rstudio.R
 
-# docker pull czeildi/erum-clean-r-code
-R -f erum-2018-clean-r-code/infra/start_multiple_rstudio.R
+# on local machine:
+# scp -R data root@ip:/home/rstudio
+
+access your containers at <ip>:20001 ...
