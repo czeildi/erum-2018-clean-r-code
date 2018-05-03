1ccf0c32c11fb4b2cd0b2a1e84b58a5e40470a4a
Ildiko Czeller <czeildi@gmail.com>
2018-05-03 10:05:50 +0200

    focus on data check in first step

--- home_location.R
+++ home_location.R
@@ -16,33 +16,7 @@ home_cities %>%
   summarize(num_coord_per_city = n_distinct(long, lat)) %>% 
   filter(num_coord_per_city > 1)
 
-# data exploration --------------------------------------------------------
-
-ordered_city_populations <- home_cities %>% 
-  group_by(country_code, city) %>% 
-  summarize(num_contact = sum(num_contact)) %>% 
-  arrange(desc(num_contact))
-
-head(ordered_city_populations) %>% 
-  inner_join(countries, by = "country_code")
-
-tail(ordered_city_populations) %>% 
-  inner_join(countries, by = "country_code")
-
-
-ordered_country_populations <- home_cities %>% 
-  group_by(country_code) %>% 
-  summarize(num_contact = sum(num_contact)) %>% 
-  arrange(desc(num_contact))
-
-head(ordered_country_populations) %>% 
-  inner_join(countries, by = "country_code")
-
-tail(ordered_country_populations) %>% 
-  inner_join(countries, by = "country_code")
-
-
 # proceed to next step ----------------------------------------------------
 
 # source("workshop_tools.R")
-# proceed_to("1_cleaned_preparation")
+# proceed_to("0_cleaned_check")
