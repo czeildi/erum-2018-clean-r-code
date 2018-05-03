d3de013fa3858def6fb872b7fb2788028a2f1056
Ildiko Czeller <czeildi@gmail.com>
2018-05-03 10:31:59 +0200

    add example for function taking ... as argument

--- home_location.R
+++ home_location.R
@@ -1,5 +1,7 @@
 library("tidyverse")
 
+source("R/clean.R")
+
 # load data ---------------------------------------------------------------
 
 # NA is valid country code, stands for Namibia, so should not be read as NA
@@ -8,39 +10,54 @@ home_cities <- read_csv("data/home_cities_frequent.csv.gz", na = "")
 
 # data preparation -------------------------------------------------------
 
-countries[!complete.cases(countries), ]
-home_cities[!complete.cases(home_cities), ]
+get_rows_with_missing_value(countries)
+get_rows_with_missing_value(home_cities)
 
-home_cities %>% 
-  group_by(country_code, city) %>% 
-  summarize(num_coord_per_city = n_distinct(long, lat)) %>% 
-  filter(num_coord_per_city > 1)
+get_cities_with_multiple_coords(home_cities)
 
 # data exploration --------------------------------------------------------
 
 ordered_city_populations <- home_cities %>% 
   group_by(country_code, city) %>% 
   summarize(num_contact = sum(num_contact)) %>% 
+  ungroup() %>% 
   arrange(desc(num_contact))
 
-head(ordered_city_populations) %>% 
-  inner_join(countries, by = "country_code")
+top_cities <- head(ordered_city_populations)
+bottom_cities <- tail(ordered_city_populations)
 
-tail(ordered_city_populations) %>% 
-  inner_join(countries, by = "country_code")
+rbind(top_cities, bottom_cities) %>% 
+  inner_join(countries, by = "country_code") %>% 
+  select(country, everything(), -iso3c, -country_code)
 
 
 ordered_country_populations <- home_cities %>% 
   group_by(country_code) %>% 
   summarize(num_contact = sum(num_contact)) %>% 
+  ungroup() %>% 
   arrange(desc(num_contact))
 
-head(ordered_country_populations) %>% 
-  inner_join(countries, by = "country_code")
+top_countries <- head(ordered_country_populations)
+bottom_countries <- tail(ordered_country_populations)
 
-tail(ordered_country_populations) %>% 
-  inner_join(countries, by = "country_code")
+rbind(top_countries, bottom_countries) %>% 
+  inner_join(countries, by = "country_code") %>% 
+  select(country, everything(), -iso3c, -country_code)
 
+
+
+# example of function grouping on variable number of columns
+max_latitude_by_groups <- function(home_cities, ...) {
+  home_cities %>% 
+    group_by(...) %>% 
+    summarize(max_latitude = max(lat)) %>% 
+    ungroup()
+}
+
+max_latitude_by_groups(home_cities)
+max_latitude_by_groups(home_cities, client_id)
+max_latitude_by_groups(home_cities, country_code, city)
+max_latitude_by_groups(home_cities, client_id, country_code, city)
 
 # proceed to next step ----------------------------------------------------
 
